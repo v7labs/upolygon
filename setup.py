729f5c43d5b001bbdefbaefe9203b077add9f61c
Simon Edwardsson <simon.edwardsson@gmail.com>
2020-06-02 15:49:10 +0100

    default to not using cython

--- setup.py
+++ setup.py
@@ -1,14 +1,21 @@
 import setuptools
-from Cython.Build import cythonize
+import os
 
 with open("README.md", "r") as f:
     long_description = f.read()
 
+USE_CYTHON = os.getenv("USE_CYTHON") in ["TRUE", "true"]
+ext = ".pyx" if USE_CYTHON else ".c"
 
 extensions = [
-    setuptools.Extension("draw_polygon", ["upolygon/draw_polygon.pyx"], extra_compile_args=["-O3", "-Wall", "-g"],),
-    setuptools.Extension("find_contours", ["upolygon/find_contours.pyx"], extra_compile_args=["-O3", "-Wall", "-g"]),
+    setuptools.Extension("draw_polygon", ["upolygon/draw_polygon" + ext], extra_compile_args=["-O3", "-Wall"]),
+    setuptools.Extension("find_contours", ["upolygon/find_contours" + ext], extra_compile_args=["-O3", "-Wall"]),
 ]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+
+    extensions = cythonize(extensions)
 
 setuptools.setup(
     name="upolygon",
@@ -19,7 +26,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/v7labs/upolygon",
-    ext_modules=cythonize(extensions),
+    ext_modules=extensions,
     install_requires=[],
     packages=setuptools.find_packages(),
     classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",],
