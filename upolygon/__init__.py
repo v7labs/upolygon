b03a008c9b9179ad28c005bd9f9edf9ddfab672a
Andrea Azzini <and.azzini@gmail.com>
2021-12-21 14:26:43 +0100

    import rle_decode and rle_encode separately

--- upolygon/__init__.py
+++ upolygon/__init__.py
@@ -1,4 +1,5 @@
 from draw_polygon import draw_polygon  # noqa
 from find_contours import find_contours  # noqa
 from simplify_polygon import simplify_polygon  # noqa
-from rle import rle_decode, rle_encode # noqa
+from rle import decode as rle_decode  # noqa
+from rle import encode as rle_encode  # noqa
