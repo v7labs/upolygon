7b963431b7a8c1f6554dad820dcc132a4be4102d
Owen Jones <owen@v7labs.com>
2023-03-28 11:15:29 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ if USE_CYTHON:
 
 setuptools.setup(
     name="upolygon",
-    version="0.1.8",
+    version="0.1.10",
     author="V7",
     author_email="simon@v7labs.com",
     description="Collection of fast polygon operations for DL",
